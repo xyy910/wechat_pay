518242f1b294d035618c8d1053e9a50198eabd4e
yang.zheng <yang.zheng@ly.com>
2024-02-21 20:42:29 +0800

    update

--- conf/config.go
+++ conf/config.go
@@ -1,1 +1,30 @@
 package conf
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"log"
+)
+
+var Conf Config
+
+type Config struct {
+	ServerPort                 string `toml:"serverPort"`
+	MchID                      string `toml:"mchID"`
+	MchCertificateSerialNumber string `toml:"mchCertificateSerialNumber"`
+	MchAPIv3Key                string `toml:"mchAPIv3Key"`
+	Appid                      string `toml:"appid"`
+	ClientKeyPath              string `toml:"clientKeyPath"`
+	PayNotify                  string `toml:"payNotify"`
+}
+
+func InitConf(path string) {
+	if _, err := toml.DecodeFile(path+"/config.toml", &Conf); err != nil {
+		log.Fatalln("InitConf err: ", err)
+	} else {
+		Conf.ClientKeyPath = path + "/wechat_pay/business_cert/apiclient_key.pem"
+		aa1, _ := json.Marshal(Conf)
+		fmt.Println("配置是：", string(aa1))
+	}
+}
--- main.go
+++ main.go
@@ -1,33 +1,53 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"io"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"wechat-pay/conf"
 	"wechat-pay/wechat_pay"
 )
 
 func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("GET current path err:", err)
+	}
+	conf.InitConf(dir)
+
 	r := gin.Default()
 	r.LoadHTMLGlob("fe/*html")
 	r.Static("/static", "./fe/dist")
-	r.Any("/ping", func(c *gin.Context) {
-		a11, _ := io.ReadAll(c.Request.Body)
-		fmt.Println("收到回调了！", string(a11))
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Any("/xiadingdan", func(context *gin.Context) {
+
+	r.Any("/prepay/demo", func(context *gin.Context) {
 		refund := context.Query("amount")
 		refund1, _ := strconv.Atoi(refund)
-		codeurl := wechat_pay.Prepay(refund1)
-		context.HTML(http.StatusOK, "html.html", gin.H{"code_url": codeurl})
+		res := wechat_pay.PrepayTest(refund1)
+		context.HTML(http.StatusOK, "html.html",
+			gin.H{"code_url": res.CodeUrl,
+				"trade_no": res.TradeNo})
 	})
 
-	r.Any("/tuikuan", func(context *gin.Context) {
+	r.POST("/prepay", func(context *gin.Context) {
+		var req wechat_pay.CreateOrderReq
+		err := context.ShouldBind(&req)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, err)
+		} else {
+			res := wechat_pay.Prepay(&req)
+			context.JSON(http.StatusOK, res)
+		}
+	})
+
+	r.Any("/order/query", func(context *gin.Context) {
+		tradeno := context.Query("tradeno")
+		res := wechat_pay.Query(tradeno)
+		context.JSON(http.StatusOK, res)
+	})
+
+	r.Any("/refund", func(context *gin.Context) {
 		refund := context.Query("amount")
 		refund1, _ := strconv.Atoi(refund)
 		tradno := context.Query("tradeno")
@@ -35,14 +55,16 @@ func main() {
 		context.JSON(http.StatusOK, res)
 	})
 
-	r.Any("/close", func(context *gin.Context) {
+	r.Any("/order/close", func(context *gin.Context) {
 		tradno := context.Query("tradeno")
 		res := wechat_pay.Close(tradno)
 		context.JSON(http.StatusOK, res)
 	})
 
-	r.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "html.html", gin.H{"title": "我是测试", "content": "123456"})
+	r.Any("/certificates", func(context *gin.Context) {
+		res := wechat_pay.DownloadCerts()
+		context.JSON(http.StatusOK, res)
 	})
-	r.Run("0.0.0.0:8181") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.Run("0.0.0.0:" + conf.Conf.ServerPort)
 }
